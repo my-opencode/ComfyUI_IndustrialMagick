75452536eb673c6e6aa274b046001e9dd9960951
Ludovic Anterieur <open.code@outlook.com>
2024-06-29 00:56:11 +0200

    IndustrialMagick node structure

--- industrial_magick.py
+++ industrial_magick.py
@@ -2,3 +2,20 @@ import folder_paths
 import os
 comfy_path = os.path.dirname(folder_paths.__file__)
 tmp_path = f'{comfy_path}/temp'
+
+class IndustrialMagick:
+    @classmethod
+    def INPUT_TYPES(cls):
+        inputs = {
+            "required": {
+                "return_image": ("BOOLEAN", {"default": False}),
+                "param_count": ("INT", {"default": 5, "min": 0, "max": 50, "step": 1})
+            }
+        }
+        for i in range(1, 50):
+            inputs["required"][f"param_{i}"] = ("STRING", {"default": ""})
+        return inputs
+
+    RETURN_TYPES = ("IMAGE","STRING","STRING",)
+    RETURN_NAMES = ("image","image_path","error",)
+    CATEGORY = "IndustrialMagick"
